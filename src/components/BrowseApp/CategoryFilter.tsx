9d12e25079ab28f5a45c52b0ea554d3720f4cf90
Jinnan Zhang <vannizhang@gmail.com>
2020-03-29 12:48:22 -0700

    update category filter

--- src/components/BrowseApp/CategoryFilter.tsx
+++ src/components/BrowseApp/CategoryFilter.tsx
@@ -10,11 +10,22 @@ interface Props {
     categorySchema: CategorySchemaDataItem;
 };
 
+interface SelectedCategory {
+    title: string;
+    subcategories: string[];
+}
+
 const CategoryFilter:React.FC<Props> = ({
     categorySchema
 })=>{
 
+    // active main catgeory that displays the all subcategory options
     const [ activeMainCategoryTitle, setActiveMainCategoryTitle ] = React.useState<string>();
+
+    const [ selectedCategory, setSelectedCategory ] = React.useState<SelectedCategory>({
+        title: '',
+        subcategories: []
+    });
 
     const toggleMainCategory = (mainCategoryTitle:string)=>{
         const newVal = mainCategoryTitle !== activeMainCategoryTitle 
@@ -22,15 +33,61 @@ const CategoryFilter:React.FC<Props> = ({
             : '';
 
         setActiveMainCategoryTitle(newVal);
-    }
+    };
 
-    const getSubCategoryFilters = (subcategories:CategorySchemaSubCategory[] )=>{
+    const toggleSubcategory = (mainCategory:CategorySchemaMainCategory, subcategoryTitle?:string)=>{
 
-        const filters = subcategories.map(subcategory=>{
+        if(mainCategory.title !== selectedCategory.title){
+
+            setSelectedCategory({
+                title: mainCategory.title,
+                subcategories: [subcategoryTitle]
+            });
+
+            return;
+        }
+
+        const index = selectedCategory.subcategories.indexOf(subcategoryTitle);
+
+        const newValues:string[] = [...selectedCategory.subcategories];
+        
+        if(index === -1){
+            newValues.push(subcategoryTitle)
+        } else {
+            newValues.splice(index, 1);
+        }
+
+        setSelectedCategory({
+            title: mainCategory.title,
+            subcategories: newValues
+        });
+    };
+
+    const getSubCategoryFilters = (mainCategory:CategorySchemaMainCategory)=>{
+
+        const style = {
+            'padding': '.1rem 0'
+        } as React.CSSProperties;
+
+        const filters = mainCategory.categories.map((subcategory, index)=>{
+
+            const selected = (
+                selectedCategory &&
+                selectedCategory.title === mainCategory.title && 
+                selectedCategory.subcategories.indexOf(subcategory.title) > -1 
+            );
+
+            const checkboxClass = selected ? 'icon-ui-checkbox-checked' : 'icon-ui-checkbox-unchecked'
+
             return (
-                <div style={{
-                    'padding': '.1rem 0'
-                }}>
+                <div 
+                    key={`subcategory-filter-${index}`}
+                    style={style}
+                >
+                    <span 
+                        className={`${checkboxClass} font-size--2`}
+                        onClick={toggleSubcategory.bind(this, mainCategory, subcategory.title)}
+                    ></span>
                     <span className='font-size--2'>{ subcategory.title }</span>
                 </div>
             );
@@ -40,9 +97,17 @@ const CategoryFilter:React.FC<Props> = ({
             <div style={{
                 'padding': '.25rem 0'
             }}>
+                <div style={style} >
+                    <span 
+                        className={`${'icon-ui-checkbox-unchecked'} font-size--2`}
+                        onClick={toggleSubcategory.bind(this, mainCategory, 'all')}
+                    ></span>
+                    <span className='font-size--2'>All</span>
+                </div>
+
                 { filters }
             </div>
-        )
+        );
     }
 
     const getCategoryFiltes = ()=>{
@@ -73,13 +138,17 @@ const CategoryFilter:React.FC<Props> = ({
 
                     {
                         isActive 
-                        ? getSubCategoryFilters(mainCategory.categories) 
+                        ? getSubCategoryFilters(mainCategory) 
                         : null
                     }
                 </div>
             )
         });
     }
+
+    React.useEffect(()=>{
+        console.log('selectedCategory', selectedCategory);
+    }, [selectedCategory])
 
     return categorySchema ? (
         <div
