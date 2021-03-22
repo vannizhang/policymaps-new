7eacb735b8835a51eb86e372cefadd9d8d820272
Jinnan Zhang <vannizhang@gmail.com>
2021-03-22 12:40:02 -0700

    add side nav btn to card carousel

--- src/components/CardCarousel/CardCarousel.tsx
+++ src/components/CardCarousel/CardCarousel.tsx
@@ -35,6 +35,31 @@ const CardCarousel:React.FC<Props> = ({
         setStartIndex(newStartIndex);
     }
 
+    const getNavBtnsAtBottom = ()=>{
+        return (
+            <div 
+                className='tablet-show'
+                style={{
+                    'display': 'flex',
+                    'alignItems': 'center',
+                    'justifyContent': 'flex-end'
+                }}
+            >
+                <div className='margin-right-half'>
+                    <NavBtn 
+                        direction={'left'}
+                        onClick={showPrev}
+                    />
+                </div>
+
+                <NavBtn 
+                    direction={'right'}
+                    onClick={showNext}
+                />
+            </div>
+        );
+    }
+
     const getCardsBlock = ()=>{
 
         if(!cardsData || !cardsData.length){
@@ -74,36 +99,52 @@ const CardCarousel:React.FC<Props> = ({
             });
 
         return (
-            <div className='block-group block-group-3-up tablet-block-group-3-up phone-block-group-1-up trailer-1'>
-                {cards}
-            </div>
-        );
-    };
-
-    return cardsData && cardsData.length ? (
-        <div>
-            { getCardsBlock() }
-            
-            <div 
-                className='tablet-show'
+            <div
                 style={{
-                    'display': 'flex',
-                    'alignItems': 'center',
-                    'justifyContent': 'flex-end'
+                    position: 'relative',
+                    // display: 'flex'
                 }}
             >
-                <div className='margin-right-half'>
+                <div 
+                    className='tablet-hide'
+                    style={{
+                        position: 'absolute',
+                        top: `calc(50% - 18px)`,
+                        left: -50
+                    }}
+                >
                     <NavBtn 
                         direction={'left'}
                         onClick={showPrev}
                     />
                 </div>
 
-                <NavBtn 
-                    direction={'right'}
-                    onClick={showNext}
-                />
+                <div className='block-group block-group-3-up tablet-block-group-3-up phone-block-group-1-up trailer-1'>
+                    {cards}
+                </div>
+
+                <div 
+                    className='tablet-hide'
+                    style={{
+                        position: 'absolute',
+                        top: `calc(50% - 18px)`,
+                        right: -50
+                    }}
+                >
+                    <NavBtn 
+                        direction={'right'}
+                        onClick={showNext}
+                    />
+                </div>
             </div>
+
+        );
+    };
+
+    return cardsData && cardsData.length ? (
+        <div>
+            { getCardsBlock() }
+            { getNavBtnsAtBottom() }
         </div>
     ) : null;
 };
