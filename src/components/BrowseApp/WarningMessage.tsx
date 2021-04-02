bab221947f4f31bfd48f05bbb942deecd731800d
Jinnan Zhang <vannizhang@gmail.com>
2021-04-02 11:42:50 -0700

    only show 'Best viewed on a tablet or PC browser' message once

--- src/components/BrowseApp/WarningMessage.tsx
+++ src/components/BrowseApp/WarningMessage.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { SiteContext } from '../../contexts/SiteContextProvider';
 
-const WarningMessage = () => {
-    const { isMobile } = React.useContext(SiteContext);
-    const [isVisible, setIsVisible] = React.useState<boolean>(true);
+const itemLey4HasSeenWarningMessage = 'policymaps.explore.seen_warning_message';
 
-    return isMobile && isVisible ? (
+const hasSeenWarningMessage = localStorage.getItem(itemLey4HasSeenWarningMessage) !== null;
+
+const WarningMessage:React.FC = () => {
+    const { isMobile } = React.useContext(SiteContext);
+    const [isVisible, setIsVisible] = React.useState<boolean>(isMobile);
+
+    React.useEffect(()=>{
+        // set has seen warning message to '1' to only show the warning message once
+        if(isVisible){
+            localStorage.setItem(itemLey4HasSeenWarningMessage, '1')
+        }
+    }, [])
+
+    return isVisible ? (
         <div className="js-modal modal-overlay is-active">
             <div className="modal-content column-6" role="dialog" aria-labelledby="modal">
 
